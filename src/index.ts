992d9467a47afacfe90092532b410b2ffdc9a337
Abraham Samuel {JavaScript Enthusiast} <abmercy035@gmail.com>
2025-04-13 16:31:23 +0100

    improving and adding new features

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import Validata from "./engine"
+import { Validata } from "./core/validataData"
+// Main entry point for the library
+export { Validata } from "./core/validataData"
+export { extend as extendValidata } from "./core/extend"
+export { extractRulesFromSchema } from "./mongoose/extractRules"
+export { validateWithSchema } from "./mongoose"
 
-	export const isInValiData = Validata
-	export const ValiData = Validata 
+// Types
+export type { ValidationResult, ValidatorFn } from "./utils/types"
+
+export const isNotValidata = Validata
+export const isInValidata = Validata
+export const isInValiData = Validata
